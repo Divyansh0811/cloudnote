75c8042146623b8d968c2cd3020242abd841b018
Divyansh Mishra <divyanshm471@gmail.com>
2021-09-24 19:36:28 +0530

    auth token & login endpoint created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -55,4 +55,40 @@ router.post(
  }
 );
 
+//Authenticating a user(Login) : /api/auth/login
+router.post('/login',[
+  body("email", "Enter a valid email").isEmail(),
+  body("password", "Password cannot be empty").exists(),
+], async (req, res) =>{
+  //if validation errors:
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+   return res.status(400).json({ errors: errors.array() });
+  }
+
+  const{email, password} = req.body;
+  try {
+    let user = await User.findOne({email})
+    if(!user){
+      return res.status(400).json({error: "Wrong credentials!"})
+    }
+
+    const comparedPassword = await bcrypt.compare(password, user.password)
+    if(!comparedPassword){
+      return res.status(400).json({error: "Wrong credentials!"})
+    }
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+ 
+    const authToken = jwt.sign(data, JWT_SECRET)
+    res.json({authToken});
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({error: "Something went wrong!"})
+  }
+})
+
 module.exports = router;
