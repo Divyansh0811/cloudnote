d04aa4607e1f4048edda395411b73562df57db23
Divyansh Mishra <divyanshm471@gmail.com>
2021-09-27 21:39:58 +0530

    some major changes

--- client/cloudnote/src/App.js
+++ client/cloudnote/src/App.js
@@ -1,23 +1,9 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import Login from "./Components/Login/Login";
-import Navbar from "./Components/Navbar/Navbar";
-import Register from "./Components/Register/Register";
+
 
 function App() {
  return (
-  <Router>
-   <Navbar />
-   <Switch>
-    <Route exact path="/">
-     <Home />
-    </Route>
-    <Route path="/register"><Register /></Route>
-    <Route path="/login"><Login /></Route>
-   </Switch>
-  </Router>
+ <div>CLOUDNOTE!!!</div>
  );
 }
 
--- client/cloudnote/src/Components/Navbar/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import './Navbar.css'
-function Navbar() {
- return <h1>Welcome to cloudnote!! your notes safe and secure on the cloud!</h1>;
-}
-
-export default Navbar;
