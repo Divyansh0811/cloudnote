0452f94c2d7ab6f33afdb46b99a780b90b083174
Divyansh Mishra <divyanshm471@gmail.com>
2021-09-29 23:36:23 +0530

    Login & signup button added in navbar

--- client/cloudnote/src/App.js
+++ client/cloudnote/src/App.js
@@ -2,15 +2,31 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import NoteState from "./Context/notes/noteState";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 function App() {
  return (
   <>
    <NoteState>
-    <Navbar />
-    <div className="container">
-     <Home />
-    </div>
+    <Router>
+     <Navbar />
+     <div className="container">
+      <Switch>
+       <Route exact path="/">
+        <Home />
+       </Route>
+       <Route exact path="/login">
+        <Home />
+       </Route>
+       <Route exact path="/signup">
+        <Home />
+       </Route>
+      </Switch>
+     </div>
+    </Router>
    </NoteState>
   </>
  );
--- client/cloudnote/src/Components/Navbar.jsx
+++ client/cloudnote/src/Components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div style={{textAlign: "center"}}>
-      <h1 style={{color: "black"}}>Welcome to Cloudnote, your notes safe and secure on the cloud.</h1>
-    </div>
-  )
-}
+ return (
+  <div className=" navbar d-flex" style={{ textAlign: "center" }}>
+   <h2  className="mx-5" style={{ color: "black" }}>
+    Welcome to Cloudnote, your notes safe and secure on the cloud.
+   </h2>
+   <form className="d-flex">
+    <Link style={{ color: "black", background: "white" }} className="btn btn-primary mx-1" to="/login" role="button">
+     Login
+    </Link>
+    <Link style={{ color: "black", background: "white" }} className="btn btn-primary mx-1" to="/signup" role="button">
+     Signup
+    </Link>
+   </form>
+  </div>
+ );
+};
 
-export default Navbar
+export default Navbar;
